e08e84d3dc043b283ddac9246697ce33a653488b
hsm1020s <hsm1020s@naver.com>
2023-11-07 15:37:37 +0900

    23개 패키지 세팅

---
+++ JAVA_Design_Pattern/src/main/java/Factory_Method/Main.java
@@ -0,0 +1,7 @@
+package Factory_Method;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Factory Method");
+    }
+}
---
+++ JAVA_Design_Pattern/src/main/java/Template_Method/Main.java
@@ -0,0 +1,7 @@
+package Template_Method;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Template_Method!");
+    }
+}
