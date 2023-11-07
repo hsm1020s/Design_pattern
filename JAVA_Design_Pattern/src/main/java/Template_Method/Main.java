1b699b77a9588bd0c5bfebab069294bb81c8aec3
hsm1020s <hsm1020s@naver.com>
2023-11-07 16:07:05 +0900

    Template Method 패턴 학습

--- JAVA_Design_Pattern/src/main/java/Template_Method/Main.java
+++ JAVA_Design_Pattern/src/main/java/Template_Method/Main.java
@@ -2,6 +2,26 @@ package Template_Method;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Template_Method!");
+
+        // 상위 클래스에서 처리의 뼈대를 결정하고 하위 클래스에서 구체적으로 그 내용을 결정하는 패턴
+        // 템플릿 메서드 패턴
+        /*
+        * AbstractDisplay 는 메서드 display만 구현된 추상클래스이다.
+        * CharDisplay 는 메서드 open, print, close 를 구현하는 클래스이다.
+        * StringDisplay 는 메서드 open,print,close 를 구현하는 클래스이다.
+        * */
+
+        // 'H'를 가진 CharDisplay 인스턴스를 하나 만든다
+        AbstractDisplay d1 = new CharDisplay('H');
+
+        // "Hello, world."를 가진 StringDisplay 인스턴스를 하나 만든다
+        AbstractDisplay d2 = new StringDisplay("Hello, world.");
+
+        // d1,d2 모두 같은 AbstractDisplay의 하위 클래스의 인스턴스이므로
+        // 상속한 display 메소드를 호출할 수 있다
+        // 실제 동작은 CharDisplay나 StringDisplay 클래스에서 정해진다
+        d1.display();
+        d2.display();
     }
 }
+
--- JAVA_Design_Pattern/src/main/java/org/example/Main.java
+++ JAVA_Design_Pattern/src/main/java/org/example/Main.java
@@ -3,5 +3,32 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /*
+        1. Template Method
+        2. Factory Method
+        3. Abstract Factory
+        4. Builder
+        5. Strategy
+        6. Composite
+        7. Iterator
+        8. Visitor
+        9. Interpreter
+        10. Flyweight
+        11. Prototype
+        12. Command
+        13. Memento
+        14. Chain of Responsibility
+        15. Singleton
+        16. State
+        17. Facade
+        18. Mediator
+        19. Observer
+        20. Bridge
+        21. Adapter
+        22. Decorator
+        23. Proxy
+
+         */
+
     }
 }
